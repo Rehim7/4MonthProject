662b52131c58e15ac8931406d7148d5a1e24314b
Rehim7 <elessar347@gmail.com>
2025-11-22 12:58:55 +0400

    Remove unused deleteById method from HabitsRepository

--- src/main/java/com/example/_monthproject/repository/HabitsRepository.java
+++ src/main/java/com/example/_monthproject/repository/HabitsRepository.java
@@ -4,7 +4,7 @@ import com.example._monthproject.model.Habits;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface HabitsRepository extends JpaRepository<Habits, Integer> {
-    void deleteById(Habits findbyd);
+    
 
     Habits findById(Long id);
 }
